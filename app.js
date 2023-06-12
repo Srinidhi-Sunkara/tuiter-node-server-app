9c2081964874f5cd98fb54dbdf266b61fce99c17
Srinidhi Sunkara <srinidhisunkara@Srinidhis-MBP.attlocal.net>
2023-06-12 10:13:47 -0500

    7

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ session({
 );
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', allowedOrigins);
+  res.setHeader('Access-Control-Allow-Origin', 'https://a5--lucent-bunny-89c745.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
